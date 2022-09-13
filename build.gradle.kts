03b1412b719d16e2d69995b0463d3491786d7c6b
Y. Z. Chen <754097987@qq.com>
2022-09-14 01:48:27 +0800

    [Update] add a build count stat.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.net.URI
+import java.io.FileOutputStream
+
 plugins {
     kotlin("multiplatform") version "1.7.10"
 }
@@ -13,5 +16,25 @@ kotlin {
                 entryPoint = "com.geno1024.builder.main"
             }
         }
+    }
+}
+tasks {
+    fun buildCount(key: String) = System.getenv("BUILD_COUNT_SERVER").takeIf { it != null }?.apply {
+        try
+        {
+            val input = URI("$this/$key").toURL().openConnection().getInputStream()
+            val output = FileOutputStream(File("$projectDir/build_$key.txt"))
+            input.copyTo(output)
+        } catch (e: java.net.UnknownHostException)
+        {
+            System.err.println("Unknown host: $e")
+        }
+    } ?: File("$projectDir/build_$key.txt").writeText("LOCAL")
+
+    val buildcount = register("buildCount") {
+        buildCount("gbuilder")
+    }.get()
+    getByName("build") {
+        dependsOn(buildcount)
     }
 }
