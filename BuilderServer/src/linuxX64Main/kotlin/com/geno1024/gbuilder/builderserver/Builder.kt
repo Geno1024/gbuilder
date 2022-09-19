b0c7cb77d574ffd859643c004eae78f72d799ec3
Y. Z. Chen <754097987@qq.com>
2022-09-19 22:28:14 +0800

    [Update] start from the server.

---
+++ BuilderServer/src/linuxX64Main/kotlin/com/geno1024/gbuilder/builderserver/Builder.kt
@@ -0,0 +1,11 @@
+package com.geno1024.gbuilder.builderserver
+
+object Builder
+{
+    fun main(args: Array<String>)
+    {
+
+    }
+}
+
+fun main(args: Array<String>) = Builder.main(args)
---
+++ BuilderServer/src/linuxX64Main/kotlin/com/geno1024/gbuilder/builderserver/EnvironmentInitializer.kt
@@ -0,0 +1,5 @@
+package com.geno1024.gbuilder.builderserver
+
+object EnvironmentInitializer
+{
+}
